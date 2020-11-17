8d67c311933a73601e1cb868069d55baf074020c
maxo30 <74299419+maxo30@users.noreply.github.com>
2020-11-17 18:35:37 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function getMaxNumber(numbersArray){
+    numbersArray.sort();
+    let lastOne = numbersArray.length;
+    alert(numbersArray[lastOne-1]);
+}
+
+let arr = [1,6,2,17,4,23,54,47,97,98,65,47,58,99,86,75];
+
+getMaxNumber(arr);
